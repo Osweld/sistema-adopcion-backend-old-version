526ccef8044e1dbaccdfd895968584d8dbe62e47
German <oswaldo101.gr@gmail.com>
2023-04-28 15:26:45 -0600

    Initial commit

---
+++ src/main/java/com/ues/dsi/sistemaadopcionbackend/SistemaAdopcionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ues.dsi.sistemaadopcionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAdopcionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAdopcionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ues/dsi/sistemaadopcionbackend/SistemaAdopcionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ues.dsi.sistemaadopcionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAdopcionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
